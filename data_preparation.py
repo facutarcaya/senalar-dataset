9628539e5100542a6766371909cdf6fdf30eea7e
SantiagoAlbarracin <santiago.albarracin@procontacto.com.mx>
2022-06-13 21:40:37 -0300

    Videos_prueba

---
+++ data_preparation.py
@@ -0,0 +1,7 @@
+from imutils import paths
+from tqdm import tqdm
+import pandas as pd 
+import numpy as np
+import shutil
+import cv2
+import os
---
+++ video_classification.py
@@ -0,0 +1,12 @@
+from tensorflow_docs.vis import embed
+from tensorflow import keras
+from imutils import paths
+
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import pandas as pd
+import numpy as np
+import imageio
+import cv2
+import os
+
